64d8a0f9e7d23392b3be976d2f95dc2f427cf7a6
Lizzie-hun <hun18015@byui.edu>
2021-10-11 17:00:44 -0600

    commit

---
+++ 05-jumper/jumper/game/director.py
@@ -0,0 +1,39 @@
+from game.console import Console
+from game.jumper import Jumper
+from game.guesser import Guesser
+
+class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to control the sequence of play.
+    
+    Stereotype:
+        Controller
+
+    Attributes:
+        console (Console): An instance of the class of objects known as Console.
+        keep_playing (boolean): Whether or not the game can continue.
+        jumper (Jumper): An instance of the class of objects known as Jumper.
+        guesser (Guesser)): An instance of the class of objects known as Guesser.
+    """
+
+    def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self.console = Console()
+        self.jumper = Jumper()
+        self.keep_playing = True
+        self.guesser = Guesser()
+        
+    def start_game(self):
+        """Starts the game loop to control the sequence of play.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.keep_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
