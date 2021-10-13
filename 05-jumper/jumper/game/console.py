5dcd43ff7201e38194fa903598ad6d3ef8ceda09
= <bentley.rebekah314@gmail.com>
2021-10-13 10:31:12 -0600

    Jumper code

---
+++ 05-jumper/jumper/game/console.py
@@ -0,0 +1,8 @@
+#import modules/classes
+
+class Console():
+    """
+
+    """
+def __init__(self):
+    pass
---
+++ jumper.py
@@ -0,0 +1,31 @@
+#import modules/classes
+
+class Jumper():
+    """
+
+
+    """
+def __init__(self):
+    
+    
+    pass
+
+def parachute(self):
+    print(" ___")
+    print("\/   \\")
+    print(" ___")
+    print("\\   \/")
+    print('\\ \/')
+    print('0')
+    print('\/|\\')
+    print('\/ \\')
+
+
+parachute()
+
+
+
+
+
+
+
