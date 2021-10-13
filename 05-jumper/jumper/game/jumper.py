220014ee82747159e745d9d801bdfa590dd2d6bb
= <bentley.rebekah314@gmail.com>
2021-10-13 11:03:59 -0600

    Should be ready to compile

--- 05-jumper/jumper/game/jumper.py
+++ 05-jumper/jumper/game/jumper.py
@@ -19,29 +19,26 @@ def __init__(self):
     self.letter = True
     #self.parachute = []
 
+
 def parachute(self):
-    for piece in self.parachute:
-        print(piece)
-
-
-def check_letter(self, letter):
-    self.letter = letter
-    if self.letter == True:
-        print('Correct')
-    elif self.letter == False:
-        self.parachute.pop[0]
-        print('Better luck next time')
+    parachute_status()
+    return self.parachute
     
 
-def parachute_status(self):
-    if bool(self.parachute):
-        for piece in self.happy_person:
-            print(piece)
-    else:
-        for piece in self.sad_person:
-            print(piece)
-            print('Game over! Try again another time and live')
+def check_letter(self, letter, parachute):
+    self.letter = letter
+    if self.letter == True:
+        parachute
+    elif self.letter == False:
+        parachute.pop[0]
+    return parachute
+    
 
+def parachute_status(self, parachute):
+    if bool(parachute):
+        return self.happy_person
+    else:
+        return self.sad_person
 
 
 
