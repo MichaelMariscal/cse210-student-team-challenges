2c6e23f2400a7942e56c5c07dda805fd91694a1e
Michael Mariscal <michael@Michaels-MacBook-Pro.local>
2021-10-13 11:28:50 -0600

    Middle

--- 05-jumper/jumper/game/guesser.py
+++ 05-jumper/jumper/game/guesser.py
@@ -4,33 +4,50 @@ class Guesser():
     """
 
     """
-def __init__(self):
-    self.word = random.choice(random_word)
-    self.guess = ""
-    pass
+    def __init__(self):
+        self.word = random.choice(random_word)
+        self.guess = ""
+        
 
-def get_word(self):
-    random_word = [sword, stock, dozen, guard, trade, major, vague, wreck ]
-    letter_guess = ""
-    store_letter = ""
-    countLetter = len(word)
-    count = 1
-    limit = 5
-    
+    def get_word(self):
+        random_word = [sword, stock, dozen, guard, trade, major, vague, wreck ]
+        letter_guess = ""
+        store_letter = ""
+        countLetter = len(word)
+        count = 1
+        limit = 5
+        choice1 = ("Do you want to add another letter if possible? (y/n):")
+        choiceY = ("y")
+        choiceN = ("n")
+
+    while count < limit:
+        letter_guess = input("Input {}" .format(count))
+
+        if letter_guess in word:
+            print ("Correct!")
+            print (input(choice1))
+        else :
+            print("Incorrect")
+            count +=1
+        if choice1 == choiceY:
+            print(input("Select a letter A-Z"))
+        else: 
+            print (letter_guess)
+            store_letter += letter_guess
+            count +=1
+
+    def display(self):
+        blanks = '_' * len(random_word)
+        for letter_guess in random_word:
+            if random_word in letter_guess:
+                blanks = blanks[:i] + random_word[i] + blanks [i+1:]
+
+        for letter in blanks:
+            print(letter, end= '')
+        print()
 
 
-while count < limit:
-    letter_guess = input("Input {}" .format(count))
-
-    if letter_guess in word:
-        print ("Correct!")
-        print (input(choice1))
-    else :
-        print("no")
-        count +=1
-    
-
-def get_guess(self):
-    self.guess = input("Select a letter A-Z: ")
+    def get_guess(self):
+        self.guess = input("Select a letter A-Z: ")
 
 #How should I go about having the system checking to see if they had the correct letter.
