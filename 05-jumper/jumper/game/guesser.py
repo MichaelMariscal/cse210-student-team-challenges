26425e8352953dc4eba29f8c391b251804f6edeb
Michael Mariscal <michael@Michaels-MacBook-Pro.local>
2021-10-13 10:31:43 -0600

    Middle

--- 05-jumper/jumper/game/guesser.py
+++ 05-jumper/jumper/game/guesser.py
@@ -1,8 +1,36 @@
 #import modules/classes
-
+import random
 class Guesser():
     """
 
     """
 def __init__(self):
+    self.word = random.choice(random_word)
+    self.guess = ""
     pass
+
+def get_word(self):
+    random_word = [sword, stock, dozen, guard, trade, major, vague, wreck ]
+    letter_guess = ""
+    store_letter = ""
+    countLetter = len(word)
+    count = 1
+    limit = 5
+    
+
+
+while count < limit:
+    letter_guess = input("Input {}" .format(count))
+
+    if letter_guess in word:
+        print ("Correct!")
+        print (input(choice1))
+    else :
+        print("no")
+        count +=1
+    
+
+def get_guess(self):
+    self.guess = input("Select a letter A-Z: ")
+
+#How should I go about having the system checking to see if they had the correct letter.
