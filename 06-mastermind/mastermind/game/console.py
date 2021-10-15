6ee7ef0b890dd54257807f9573740a1e4f7e3966
Lizzie-hun <hun18015@byui.edu>
2021-10-15 13:06:06 -0600

    my commit

---
+++ 06-mastermind/mastermind/game/console.py
@@ -0,0 +1,10 @@
+class Console:
+
+    def read(self, prompt):
+        return input(prompt)
+    
+    def read_num(self,num):
+        return int(input(num))
+
+    def write(self,text):
+        print(text)
