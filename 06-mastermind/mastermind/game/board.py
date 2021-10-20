0bd291b53b8f14eeff92d2d5e3c20845d2b3787a
Lizzie-hun <hun18015@byui.edu>
2021-10-19 18:38:52 -0600

    fixed board class

--- 06-mastermind/mastermind/game/board.py
+++ 06-mastermind/mastermind/game/board.py
@@ -2,22 +2,25 @@ class Board:
     """
     creates a string for the board that will appear on the screen
     """
+
+    #defines the attributes for the board class
     def __init__(self):
         self.board = []
-        self.player_message = ""
+        self.message = ""
         self.guess = "What is your guess? "
         
-
+    #sets board equal to the list so it can be printed
     def define_board(self, players, code, correct):
         for player in players:
             for x in range(player+2):
                 if x == 0 or x == player + 1:
                     one = "-" * 20
                 else:
-                    one = "Player " + players[x-1] ": " + code ", " + correct
+                    one = "Player " + players[x-1] + ": " + code + ", " + correct
                 self.board.append(one)
         return self.board 
 
+    #sets message equal to a string using current_player from the switch class
     def define_message(self, current_player):
-        self.player_message = current_player.title() + "'s turn : "
+        self.message = current_player.capitalize() + "'s turn : "
     
--- 06-mastermind/mastermind/game/code.py
+++ 06-mastermind/mastermind/game/code.py
@@ -1,6 +1,22 @@
+import random
+
 class Code:
     """
     creates, stores, and edits a secret code for each player
     """
+    #defines the attributes for this class
     def __init__(self):
+        self.secret = []
+        self.correct = []
+
+    #sets a secret code for each player in the game
+    def secret_code(self, players):
+        for player in players:
+            code = random.randint
+            self.secret.append(code)
+    
+    def check_code(self):
         pass
+
+    def is_correct(self):
+        passs
