a6042daf1c631af88fcb0fa14d88f4b93d648400
= <bentley.rebekah314@gmail.com>
2021-10-30 17:53:58 -0600

    Added code

--- 07-speed/speed/game/display.py
+++ 07-speed/speed/game/display.py
@@ -1,7 +1,19 @@
 from game import constants
 from game.actor import Actor
 from game.point import Point
+import random
 
 class Display:
+    """
+    Display words going across the screen, control velocity and position.
+    """
     def __init__(self):
-        pass
+        self.word_list = []
+        self.word = ""
+
+
+    def choose_word(self, file):
+        self.word = random.choice(open(file).read().split()).strip()
+
+    
+        
