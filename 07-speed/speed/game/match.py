4f915acb5788e444409f8348eca6f28e6e1f15ad
Michael Mariscal <michael@michaels-mbp.lan>
2021-11-01 15:37:36 -0600

    test

--- 07-speed/speed/game/match.py
+++ 07-speed/speed/game/match.py
@@ -1,3 +1,6 @@
 class Match:
     def __init__(self):
         pass
+
+# Looping through the display list. 2 attributes from the other classes. 
+# Keep track of the length of the word and add or subtract points for each letter. Score board will grab the points at the end.
