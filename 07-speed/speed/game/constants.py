6184f3220d28f63d8429abcbff8911f661c6d228
Lizzie-hun <hun18015@byui.edu>
2021-11-01 11:11:36 -0600

    min and max

--- 07-speed/speed/game/constants.py
+++ 07-speed/speed/game/constants.py
@@ -8,6 +8,9 @@ FRAME_RATE = 30
 DEFAULT_SQUARE_LENGTH = 20
 DEFAULT_FONT_SIZE = 16
 
+MAX_VELOCITY = -5
+MIN_VELOCITY = -1
+
 DEFAULT_TEXT_OFFSET = 5
 PATH = os.path.dirname(os.path.abspath(__file__))
 LIBRARY = open(PATH + "/words.txt").read().splitlines()
