4933dc1633cf763d94143a81cfb9b16bde4a0d5a
Michael <mmar18@byui.edu>
2021-11-05 15:06:56 -0600

    testing1

--- 07-speed/speed/game/match.py
+++ 07-speed/speed/game/match.py
@@ -6,30 +6,37 @@ class Match(Actor):
     
     def __init__(self):
         self.screen_list = []
-        self.get_text = ""
+        
         self.words = []
-        self._points = 0
-        self.set_text(f"Score: {self._points}")
-        self.set_text(f"Buffer: {self.words}")
+        
+        
         #position = Point(1, constants.MAX_Y)
 
 
 
     def check_word(self, letter):
-         self.words += letter
-         self.set_text(f"Buffer: {self.words}")
+         _screen_list = [{self.screen_list}]
+         all_text = input(" ")
+         for item in _screen_list:
+             if item in all_text:
+                 print("")
+         
+         
+         
+         
+         #matches = [FALSE]*len
    
 
 
 
-    def add_points(self,points):
-        self._points += points
-        self.set_text(f"Score: {self._points}")
+    #def add_points(self,points):
+        #self._points += points
+        #self.set_text(f"Score: {self._points}")
 
     #def add_points(self,points):
         #self._points += points
         #self.set_text(f"Score: {self._points}")
-        #int.point = len(word)
+        #int._points = len(word)
 
     #def grab_word(self):
             #return self.words
@@ -37,7 +44,8 @@ class Match(Actor):
 
 
 
- #if self.get_text in self.screen_list:
+ #if self._buffer in self.screen_list:
+ #clear buffer
 # How to set position for the score on the top left
 #int.point = len(word)
 #match = self.screen_list()
